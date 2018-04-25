12623ce4daa854fa9dd41917890ace80196bca01
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-04-25 13:52:57 -0400

    script for initial velvet assembly of COI reads, and pulling out the longest contig from that assembly. this is the intial sequence that will be used in aTRAM.

---
+++ initialVelvet.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# velvet assembly of mapped reads by locus
+# COI used for testing
+
+# run velvet on the reads that mapped to COI and pull out the longest contig that was assembled
+# this longest contig will be the input to aTRAM
+
+for f in *COI.fq;
+do
+velveth $f.initial 29 -short -fastq $f;
+velvetg $f.initial;
+python getLongest.py -f $f.initial/contigs.fasta -o $f.initial.longest.fasta;
+done
+
