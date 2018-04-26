3abdd47b3494083228360959c390a7ddfc5fd268
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-04-26 12:58:57 -0400

    fixed bug

--- preprocess4atram.sh
+++ preprocess4atram.sh
@@ -8,7 +8,7 @@
 for f in *.fastq;
 do
 	if [  -e ${f%.*}.atram_preprocessor.log  ];
-	    echo Reads already preprocessed! See ${f%.*}.atram_preprocessor.log;
+	    then echo Reads already preprocessed! See ${f%.*}.atram_preprocessor.log;
 	else    
         atram_preprocessor.py -b ${f%.*} --mixed-ends $f;
     fi;    
