c78097067054a990e6d9e425fb50574b9a2a4cf3
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-04-30 14:25:19 -0400

    added full path for Exonerate, because it's not in the path in Makaira

--- exonerate4COI.sh
+++ exonerate4COI.sh
@@ -2,5 +2,5 @@
 
 for f in *.atram.fasta;
 do
-exonerate --model coding2coding --geneticcode 2 -q $f -t Reference_barcode.fasta --ryo ">%qi%qd\n%qas\n" --showcigar F --showvulgar F --showalignment F --showsugar F --showquerygff F --showtargetgff F --bestn 1 > ${f%.*}.exonerate.fasta;
+/storage/home/fishlab/FishLifeData/lily-scripts/exonerate-2.2.0/bin/exonerate --model coding2coding --geneticcode 2 -q $f -t Reference_barcode.fasta --ryo ">%qi%qd\n%qas\n" --showcigar F --showvulgar F --showalignment F --showsugar F --showquerygff F --showtargetgff F --bestn 1 > ${f%.*}.exonerate.fasta;
 done
