6b6b1e05349800324338a2ed1b6581f9eb548d33
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-04-30 16:55:13 -0400

    Updated script to parse through different scenarios of hits

--- barcodeChecker.py
+++ barcodeChecker.py
@@ -13,20 +13,59 @@ species = args.blast.split(".")[0]
 
 BlastInput = open(args.blast)
 
-BestHit = BlastInput.readline()
+hits = []
+idents = []
+
+for line in BlastInput:
+	barcodeSeq = line.split("\t")[1]
+	percID = line.split("\t")[2]
+	hits.append(barcodeSeq)
+	idents.append(percID)
 
 BlastInput.close()
 
-table = BestHit.split("\t")
 
-barcodeSeq = table[1]
+dict = dict(zip(hits,idents))
 
-barcodeSP = barcodeSeq.split("|")[1]
+# species ID matches and percent identity is >97%
+bestMatch = []
 
-percID = table[2]
+# for matches that have high identity (>97%), but list a different species
+goodMatches = []
 
-if barcodeSP in species:
-	print "Barcode match! "+species+" matches "+barcodeSeq+" with "+str(percID)+"% identity match to BOLD COI database."
-else:
-	print "Mismatch! Species labeled "+species+" but closest COI match is "+barcodeSP+" with "+str(percID)+"% identity."
+# for matches >90%, 
+mediumMatches = []
+
+# for matches <90%
+badMatches = []
+
+for item in dict.keys():
+	barcodeSP = item.split("|")[1]
+	if barcodeSP in species and dict[item] >= 97.0:
+		bestMatch.append(item)
+	elif barcodeSP not in species and dict[item] >= 97.0:
+	    goodMatches.append(item)	
+	elif dict[item] >= 90.0 and dict[item] < 97.0:
+		mediumMatches.append(item)
+	elif dict[item] < 90.0:
+		badMatches.append(item)		
+
+if len(bestMatch) > 0:
+	key = bestMatch[0]
+	perc = dict[key]
+	print "Barcode match! "+species+" matches "+key+" with "+perc+"% identity."
+elif len(goodMatches) > 0:
+	key = goodMatches[0]
+	perc = dict[key]
+	print "No exact match. Best hit for "+species+" matches "+key+" with "+perc+"% identity."
+elif len(mediumMatches) > 0:
+	key = mediumMatches[0]
+	perc = dict[key]
+	print "No exact match. Best hit for "+species+" matches "+key+" with "+perc+"% identity."
+elif len(badMatches) > 0:
+	key = badMatches[0]
+	perc = dict[key]
+	print "No exact match. Best hit for "+species+" matches "+key+" with "+perc+"% identity."
+
+		
 		
