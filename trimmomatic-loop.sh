729ad4b6822f0dc3a60aace583e3c7e29a66204c
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-05-10 15:21:07 -0400

    fixed error

--- trimmomatic-loop.sh
+++ trimmomatic-loop.sh
@@ -6,7 +6,7 @@ for f in *.fastq;
 do
 	if [  -e $f.trimlog  ];
 		then echo Reads already trimmed.;
-	else;
+	else
 		trimmomatic SE -threads 4 -phred33 -trimlog $f.trimlog $f ${f%.*}.trimmed.fastq ILLUMINACLIP:adapters.fa:2:30:10 LEADING:5 TRAILING:5 SLIDINGWINDOW:4:15 MINLEN:31;
 	fi;
 done		
