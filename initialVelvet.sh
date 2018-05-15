babee6751d0911af7ad86744da3bcd40a9181f8d
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-05-15 17:05:55 -0400

    changed velvet assembly step so that getting the longest contig is not part of the process. Leaves all velvet output in the working directory. 'combining contigs' will be a separate step.

--- initialVelvet.sh
+++ initialVelvet.sh
@@ -5,26 +5,17 @@
 
 # run velvet on the reads that mapped to COI and pull out the longest contig that was assembled
 # this longest contig will be the input to aTRAM
-# getLongest.py needs to be in the path
+
 
 
 # assemble mapped reads into a starter contig using velvet, and get the longest contig
-for f in *COI.fq;
+for f in *.fq;
 do
-	if [  -e $f.initial.longest.fasta  ];
+	if [  -e $f.initial  ];
 		then echo Initial assembly of $f reads already completed.;
 	else
 		velveth $f.initial 29 -short -fastq $f;
 		velvetg $f.initial;
-		python2.7 getLongest.py -f $f.initial/contigs.fa -o $f.initial.longest.fasta;		
 	fi;
 done
 
-
-# remove other velvet output (not used in future steps) if fasta file exists
-for f in *.initial;
-do
-	if [  -e $f.longest.fasta  ];
-		then rm -r $f;
-	fi;
-done
