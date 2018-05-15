5d99407edea7ed9e30a52f9f96fdad1f4dc3597b
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-05-15 16:41:05 -0400

    added step to rename trimmed.fq files back to fastq

--- preprocess4atram.sh
+++ preprocess4atram.sh
@@ -4,6 +4,8 @@
 # uses trimmed reads
 # this assumes mixed end data. change if you're using single-end or two paired-end files. see aTRAM docs.
 
+rename .trimmed.fq .trimmed.fastq *.fq
+
 for f in *.trimmed.fastq;
 do
 	if [  -e ${f%.*}.atram_preprocessor.log  ];
