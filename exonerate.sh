e2a4768f323eff18d0efdd92a513d917be3481cc
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-06-14 11:01:52 -0400

    changed loop so it will only run when aTRAM output is present

--- exonerate.sh
+++ exonerate.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
-
-
 cp /storage/home/fishlab/FishLifeData/lily-scripts/FishLifeExonCapture/ReadingFrames/*.fasta .
 
 for f in E*.fasta;
 do
-	for i in exon*${f%.*}*filtered*fasta;
-	do
-		if [  -e exon*${f%.*}*filtered*fasta  ];
-		then 
+	if [  -e exon*${f%.*}*filtered*fasta  ];
+	then
+		for i in exon*${f%.*}*filtered*fasta;
+		do
 		exonerate --model coding2coding -q $i -t $f --ryo ">%qi%qd\n%qas\n" --showcigar F --showvulgar F --showalignment F --showsugar F --showquerygff F --showtargetgff F --bestn 1 > ${i%.*}.exonerate.fasta;
-		fi;
-	done;
+		done;
+	fi;
 done;
 
 rm E*.fasta
