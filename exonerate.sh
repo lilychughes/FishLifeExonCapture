5d89630e3252480877411982e7ee60cdd529b19e
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-06-14 16:28:13 -0400

    fixed name error

--- exonerate.sh
+++ exonerate.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 cp /storage/home/fishlab/FishLifeData/lily-scripts/FishLifeExonCapture/ReadingFrames/*.fasta .
 
-for f in E*.fasta;
+for j in *.db;
 do
-	if [  -e exon*${f%.*}*filtered*fasta  ];
-	then
-		for i in exon*${f%.*}*filtered*fasta;
-		do
-		exonerate --model coding2coding -q $i -t $f --ryo ">%qi%qd\n%qas\n" --showcigar F --showvulgar F --showalignment F --showsugar F --showquerygff F --showtargetgff F --bestn 1 > ${i%.*}.exonerate.fasta;
-		done;
-	fi;
+	for f in E*.fasta;
+	do	
+		if [  -e exon.${j%.*.*.*}_${j%.*.*.*}.trimmed.fastq.${f%.*}.fq.initial.combined.filtered_contigs.fasta  ];
+		then
+			for i in exon*${f%.*}*filtered*fasta;
+			do
+				exonerate --model coding2coding -q $i -t $f --ryo ">%qi%qd\n%qas\n" --showcigar F --showvulgar F --showalignment F --showsugar F --showquerygff F --showtargetgff F --bestn 1 > ${i%.*}.exonerate.fasta;
+			done;	
+		fi;
+	done;
 done;
 
