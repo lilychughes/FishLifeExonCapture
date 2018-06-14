bfd0cfd6a7ecbcfe2412bf1ea9bb734cc734c2b0
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-06-14 10:34:41 -0400

    fixed error with genetic code & target vs. query

--- exonerate.sh
+++ exonerate.sh
@@ -9,7 +9,7 @@ do
 		then 
 		echo Reading Frames found for $i;
 		else
-		exonerate --model coding2coding --geneticcode 2 -q $i -t $f --ryo ">%qi%qd\n%qas\n" --showcigar F --showvulgar F --showalignment F --showsugar F --showquerygff F --showtargetgff F --bestn 1 > ${i%.*}.exonerate.fasta;
+		exonerate --model coding2coding -t $i -q $f --ryo ">%qi%qd\n%qas\n" --showcigar F --showvulgar F --showalignment F --showsugar F --showquerygff F --showtargetgff F --bestn 1 > ${i%.*}.exonerate.fasta;
 		fi;
 	done;
 done;
