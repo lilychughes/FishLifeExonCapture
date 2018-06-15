583a9331dffb85ed1ad6c7efec370b910babd47d
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-06-15 11:51:44 -0400

    added statement to check if reading frames already exist

--- exonerate.sh
+++ exonerate.sh
@@ -9,7 +9,12 @@ do
 		then
 			for i in exon*${f%.*}*filtered_contigs.fasta;
 			do
-				exonerate --model coding2coding -q $i -t $f --ryo ">%qi%qd\n%qas\n" --showcigar F --showvulgar F --showalignment F --showsugar F --showquerygff F --showtargetgff F --bestn 1 > ${i%.*}.exonerate.fasta;
+				if [  -e ${i%.*}.exonerate.fasta  ];
+				then
+					echo reading frames already found $i;
+				else		
+					exonerate --model coding2coding -q $i -t $f --ryo ">%qi%qd\n%qas\n" --showcigar F --showvulgar F --showalignment F --showsugar F --showquerygff F --showtargetgff F --bestn 1 > ${i%.*}.exonerate.fasta;
+				fi;
 			done;	
 		fi;
 	done;
