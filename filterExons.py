d970f1b7c031e56d5f8edf60d283dccd7424624b
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-08-08 14:15:26 -0400

    combined filtering with stop codon and strand orientation checking

--- filterExons.py
+++ filterExons.py
@@ -10,55 +10,63 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
-parser = argparse.ArgumentParser(description="Requires python 2.7 and Biopython. Filters the longest, highest coverage exon from the exonerate output, and renames the sequence simply as >Taxon|Exon")
+parser = argparse.ArgumentParser(description="Requires python 2.7 and Biopython. Filters the longest, highest coverage exon from the exonerate output, and renames the sequence simply as >Taxon")
 parser.add_argument('-f', '--fasta' , dest = 'fasta' , type = str , default= None , required= True, help = 'Fasta alignment to prune')
 parser.add_argument('-o', '--output', dest = 'output', type = str, default = None, required = True, help = 'Name of output file')
 parser.add_argument('-t', '--taxon', dest = 'taxon', type = str, default = None, required = True, help = 'Name of taxon')
-parser.add_argument('-e', '--exon', dest = 'exon', type = str, default = None, required = True, help = 'Name of locus')
 parser.add_argument('-l', '--length', dest = 'length', type = int, default = 100, required = True, help = 'Minimum sequence length to write to file')
 parser.add_argument('-c', '--coverage', dest = 'coverage', type = float, default = 5.0, required = True, help = 'Minimum coverage required to write a sequence to a file')
 
 args, unknown = parser.parse_known_args()
 
-seqID = args.taxon+"|"+args.exon
 
+# read in sequences
 input = open(args.fasta)
 records = list(SeqIO.parse(input, "fasta"))
 input.close()
 records.sort(key=lambda r: -len(r))
 
-if len(records) == 1 and len(records[0].seq) >= args.length:
-	seq0 = records[0]
-	desc0 = seq0.description.split(" ")[1]
-	coverage0 = (float(desc0.split("_")[6]))
-	if coverage0 > args.coverage:
-		filteredSeq = SeqRecord(records[0].seq, id = seqID, description = records[0].description)
-		SeqIO.write(filteredSeq, args.output, "fasta")
-		print("Filtered exon for "+args.taxon+" "+args.exon)
-	else:
-		print("No sequences passed filters for "+args.taxon+" locus "+args.exon)
-elif len(records) == 1 and len(records[0].seq) < args.length:
-	print("No sequences passed filters for "+args.taxon+" locus "+args.exon)
-elif len(records) > 1:		
-	seq0 = records[0]
-	seq1 = records[1]
-	desc0 = seq0.description.split(" ")[1]
-	desc1 = seq1.description.split(" ")[1]
-	coverage0 = (float(desc0.split("_")[6]))
-	coverage1 = (float(desc1.split("_")[6]))
-	if len(seq0.seq) < args.length and len(seq1.seq) < args.length:
-		print("No sequences passed filters for "+args.taxon+" locus "+args.exon)
-	elif coverage0 < args.coverage and coverage1 < args.coverage:
-		print("No sequences passed filters for "+args.taxon+" locus "+args.exon)	
-	elif coverage0 >= coverage1 and len(seq0.seq) >= len(seq1.seq):
-		filteredSeq = SeqRecord(seq0.seq, id = seqID, description = seq0.description)
-		SeqIO.write(filteredSeq, args.output, "fasta")
-		print("Filtered exon for "+args.taxon+" "+args.exon)
-	elif coverage1 > coverage0 and len(seq1.seq) >= len(seq0.seq):
-		filteredSeq = SeqRecord(seq1.seq, id = seqID, description = seq1.description)
-		SeqIO.write(filteredSeq, args.output, "fasta")
-		print("Filtered exon for "+args.taxon+" "+args.exon)
-	elif coverage0 < coverage1 and len(seq0.seq) > len(seq1.seq):
-		filteredSeq = SeqRecord(seq0.seq, id = seqID, description = seq0.description)
-		SeqIO.write(filteredSeq, args.output, "fasta")
-		print("Filtered exon for "+args.taxon+" "+args.exon)
+# filter just the sequences in the correct orientation (known from the reference sequence used with exonerate)
+oriented = []
+
+for record in records:
+	start = record.description.split("-")[0]
+	end = record.description.split("-")[1]
+	if end > start:
+		oriented.append(record)	
+
+# filter for stop codons 
+
+noStops = []
+
+for record in oriented:
+	if len(record.seq) % 3 == 0 and "*" not in record.seq.translate():
+		noStops.append(record)
+
+
+# filter for length
+longest = []
+
+if len(noStops) > 0:
+	for record in noStops:
+		if len(record.seq) >= args.length:
+			longest.append(record)
+
+# filter for coverage (assumes Velvet-style header)
+
+covered = []
+
+if len(longest) > 0:
+	for record in longest:
+		seqCoverage = float(record.id.split("-")[6])
+		if seqCoverage >= args.coverage:
+			covered.append(record)
+
+# write the output 
+# chooses longest sequence over highest coverage, may adjust this in the future, or make it optional to choose sequence with higher coverage instead
+
+if len(covered) < 1:
+	print("No sequences passed filters in "+args.fasta) 			
+elif len(covered) >= 1:
+	filteredSeq = SeqRecord(covered[0].seq, id=args.taxon, description='')
+	SeqIO.write(filteredSeq, args.output, "fasta")
