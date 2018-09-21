bbab3be0c5975e0814db39acc60913a3dfc5152d
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-09-21 11:33:31 -0400

    Updated to remove sequences with too many gaps

--- gapCleaner.py
+++ gapCleaner.py
@@ -7,10 +7,12 @@ import os
 from Bio import AlignIO
 from Bio import Seq
 from Bio.SeqRecord import SeqRecord
+from Bio.Align import MultipleSeqAlignment
 
-parser = argparse.ArgumentParser(description="Requires python 2.7 and Biopython. Removes columns with single taxon insertions.")
+parser = argparse.ArgumentParser(description="Requires python 2.7 and Biopython. Removes columns with single taxon insertions by default. If -c is specified, this will also remove sequences with more than this percentage of gaps")
 parser.add_argument('-f', '--fasta' , dest = 'fasta' , type = str , default= None , required= True, help = 'Fasta alignment to prune')
 parser.add_argument('-o', '--output', dest = 'output', type = str, default = None, required = True, help = 'Name of output file')
+parser.add_argument('-c', '--coverage', dest = 'coverage', type = float, default = 0.90, required = False, help = 'Maximum gap percentage allowed')
 args, unknown = parser.parse_known_args()
 
 # read in the alignment in fasta format
@@ -19,18 +21,30 @@ alignment = AlignIO.read(args.fasta,"fasta")
 # get the number of columns in the alignment
 length = alignment.get_alignment_length()
 
+# drop taxa with more than the allowed percentage of gaps
+goodTaxa = []
+
+for record in alignment:
+	seq = record.seq
+	gaps = seq.count("-")
+	if gaps < (length*args.coverage):
+		goodTaxa.append(record)
+
+TaxAlignment =  MultipleSeqAlignment(goodTaxa)
+
+
 # find the columns that are not composed of mostly gaps (where three or fewer taxa have sequence data or 'NNNN' sequences)
 goodColumns = []
 
 for x in range(0,length):
-	column = alignment[:,x]
-	if column.count("-") < (len(alignment)-3):
-		slice = alignment[:,x:x+1]
+	column = TaxAlignment[:,x]
+	if column.count("-") < (len(TaxAlignment)-3):
+		slice = TaxAlignment[:,x:x+1]
 		goodColumns.append(slice)
 
 # make an empty alignment to populate
 		
-newAlignment = alignment[:,0:0]
+newAlignment = TaxAlignment[:,0:0]
 
 for column in goodColumns:
 	newAlignment = newAlignment+column
