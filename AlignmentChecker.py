4c2291a023b6e35ed8484f406c6db478c3e4c19b
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-09-25 15:22:44 -0400

    Updated to be less sensitive to gaps

--- AlignmentChecker.py
+++ AlignmentChecker.py
@@ -8,9 +8,12 @@ from Bio import Seq
 from Bio import Phylo
 from Bio.Phylo.TreeConstruction import DistanceCalculator
 from Bio import AlignIO
+from Bio.Align import MultipleSeqAlignment
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
 import numpy
 
-parser = argparse.ArgumentParser(description='Requires python 2.7 and Biopython. Flags sequences based on distance')
+parser = argparse.ArgumentParser(description='Requires python 2.7 and Biopython. Flags nucleotide sequences based on distance')
 parser.add_argument('-f', '--fasta' , dest = 'fasta' , type = str , default= None , required= True, help = 'Fasta alignment to prune')
 args, unknown = parser.parse_known_args()
 
@@ -18,10 +21,22 @@ args, unknown = parser.parse_known_args()
 # read in the fasta-formatted alignment
 alignment = AlignIO.read(args.fasta, "fasta")
 
+# replace Ns with gaps (distance calculator doesn't like Ns)
+
+NewSeqs = []
+
+for record in alignment:
+	original = str(record.seq).replace("N","-")
+	new = Seq(original)
+	NewRecord = SeqRecord(new, id = record.id, description = '')
+	NewSeqs.append(NewRecord)
+
+NewAlignment = MultipleSeqAlignment(NewSeqs)	
+
 # set the method of distance calculation 
-calculator = DistanceCalculator('identity')
+calculator = DistanceCalculator('trans')
 # get pairwise distances for all sequences in the alignment
-dm = calculator.get_distance(alignment)
+dm = calculator.get_distance(NewAlignment)
 
 # get a list of sequence names
 names = []
@@ -42,6 +57,6 @@ dictionary = dict(zip(names,meanDist))
 
 # print sequence names that have more than two standard deviations difference in their mean distance
 for item in dictionary.keys():
-	if float(dictionary[item]) > ((numpy.mean(meanArray))+(numpy.mean(meanArray)+((numpy.std(meanArray))*2))):
+	if float(dictionary[item]) >= (numpy.mean(meanArray)+((numpy.std(meanArray)*2))):
 		print(item)
 		
