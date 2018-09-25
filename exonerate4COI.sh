fc51827d6e1eb8b2b16a7c9bf7c81597ae87c109
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-09-25 13:35:16 -0400

    removed outdated exonerate files

--- exonerate4COI.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for f in *.atram.fasta;
-do
-/storage/home/fishlab/FishLifeData/lily-scripts/exonerate-2.2.0/bin/exonerate --model coding2coding --geneticcode 2 -q $f -t Reference_barcode.fasta --ryo ">%qi%qd\n%qas\n" --showcigar F --showvulgar F --showalignment F --showsugar F --showquerygff F --showtargetgff F --bestn 1 > ${f%.*}.exonerate.fasta;
-done
