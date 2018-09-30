3be04fbcab5ca3f8a81aef2846d2df9729a0911a
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-09-30 14:12:22 -0400

    updated

--- trimmomatic-loop.sh
+++ trimmomatic-loop.sh
@@ -15,7 +15,10 @@ then
 	java -jar /c1/apps/trimmomatic/Trimmomatic-0.33/trimmomatic-0.33.jar SE -threads 4 -phred33 -trimlog $directory.trimlog $directory.fastq $directory.trimmed.fastq ILLUMINACLIP:../adapters.fa:2:30:10 LEADING:5 TRAILING:5 SLIDINGWINDOW:4:15 MINLEN:31;
 	echo Trimming completed > ../$directory.trimming.txt;
 	tar -cvzf $directory.fastq.tar.gz $directory.fastq;
+	if [  -s $directory.fastq.tar.gz  ];
+	then
 	rm $directory.fastq;
+	fi;
 	echo Untrimmed reads compressed $directory.fastq.tar.gz  > ../$directory.trimming.txt;
 	cd ../;	
 	fi;
