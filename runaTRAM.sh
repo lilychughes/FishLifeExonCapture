136f57ee4c1576ff50852cbb175ea9087acc2fe4
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-09-30 14:18:26 -0400

    updated aTRAM script

---
+++ runaTRAM.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# assumes atram is in your path!
+# using velvet as the assembler for aTRAM; velvet must also be in the path
+
+for directory in *;
+do
+if [  -d $directory  ];
+then
+	if [  ! -e $directory.aTRAM.txt  ];
+	then
+		cd $directory;
+		echo $directory aTRAM assembly started > ../$directory.aTRAM.txt
+		for f in *.initial.combined.fa;
+			do
+				if [  ! -e ${f%.*.*.*.*.*.*.*}.${f%.*}.atram.log  ];
+					then 
+					atram.py -b ${f%.*.*.*.*.*.*} -q $f -a velvet -o exon -i 10;
+				fi;
+			done;
+		cd ../;
+		echo aTRAM assembly completed $directory >> $directory.aTRAM.txt;
+	fi;	
+fi;
+done	
+	 	
+
+	
+# may add a clean-up step to remove extra aTRAM files
