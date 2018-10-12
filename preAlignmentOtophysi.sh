37bbcd20164188d7cf6fed47a8ef93af7d36dabd
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-10-12 15:51:59 -0400

    fixed error

--- preAlignmentOtophysi.sh
+++ preAlignmentOtophysi.sh
@@ -11,9 +11,16 @@ do
 	do
 		if [  -d $directory  ];
 		then
-		cat $directory/exon*$exon*filtered.fa >> Alignments/$exon.unaligned.fasta;
-		cat $directory/exon*Mito*filtered.fa >> Alignments/G0001.unaligned.fasta
+		cat $directory/exon*$exon*exonerate_filtered.fa >> Alignments/$exon.unaligned.fasta;		
 		fi;	
 	done;
 done < ../FishLifeExonCapture/OtophysiExons.txt
 
+
+for directory in *;
+do
+	if [  -d $directory  ];
+	then
+	cat $directory/exon*Mito*exonerate_filtered.fa >> Alignments/G0001.unaligned.fasta;
+	fi;
+done	
