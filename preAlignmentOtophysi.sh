05b58d6256d2e231e1235c81b080072ae5b1fcb7
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-10-12 14:03:38 -0400

    adjusted for Otophysi set of markers

---
+++ preAlignmentOtophysi.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# make a directory to store alignments
+mkdir Alignments/
+
+
+# cat individual exons into files for alignment
+while read -r exon;
+do
+	for directory in *;
+	do
+		if [  -d $directory  ];
+		then
+		cat $directory/*$exon*fa >> Alignments/$exon.unaligned.fasta;
+		fi;
+	cat $directory/*Mito*fa Alignments/G0001.unaligned.fasta
+	done;
+done < ../FishLifeExonCapture/OtophysiExons.txt
+
