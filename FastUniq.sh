a26a4b90f376ccd92c10afa756ca24d64e21c3c5
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-10-28 19:23:35 -0400

    added structure to write log files

--- FastUniq.sh
+++ FastUniq.sh
@@ -4,12 +4,16 @@ for directory in *;
 do
 if [  -d $directory  ];
 then
+if [  ! -e $directory.dedup.txt  ];
+then
+echo $directory deduplication started > $directory.dedup.txt;
 cd $directory/;
 gunzip *trimmed.fastq;
 ls *.trimmed.fastq > inputlist.txt;
 ../../FastUniq/source/fastuniq -i inputlist.txt -o ${directory%/}_R1.trimmed_dedup.fastq -p ${directory%/}_R2.trimmed_dedup.fastq;
 gzip *trimmed.fastq;
 cd ../;
-echo $directory deduplication completed;
+echo $directory deduplication completed > $directory.dedup.txt;
+fi;
 fi;
 done
