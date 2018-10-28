5f56464e5f53182ade13dc98a828854e1ade3c0d
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-10-28 19:42:26 -0400

    fixed error in gzip

--- FastUniq.sh
+++ FastUniq.sh
@@ -4,13 +4,15 @@ for directory in *;
 do
 if [  -d $directory  ];
 then
+taxon=$(echo ${directory%/})
+echo $taxon;
 if [  ! -e $directory.dedup.txt  ];
 then
 echo $directory deduplication started > $directory.dedup.txt;
 cd $directory/;
-gunzip *trimmed.fastq;
+gunzip *trimmed.fastq.gz;
 ls *.trimmed.fastq > inputlist.txt;
-../../FastUniq/source/fastuniq -i inputlist.txt -o ${directory%/}_R1.trimmed_dedup.fastq -p ${directory%/}_R2.trimmed_dedup.fastq;
+../../FastUniq/source/fastuniq -i inputlist.txt -o $taxon_R1.trimmed_dedup.fastq -p $taxon_R2.trimmed_dedup.fastq;
 gzip *trimmed.fastq;
 cd ../;
 echo $directory deduplication completed > $directory.dedup.txt;
