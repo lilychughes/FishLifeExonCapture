d58dce62d753cea678fccecf42906d0834a9f1d6
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-10-28 19:05:36 -0400

    fixed error

--- runaTRAM.sh
+++ runaTRAM.sh
@@ -17,7 +17,7 @@ then
 			do
 				if [  ! -e ${f%.*.*.*.*.*.*.*}.${f%.*}.atram.log  ];
 					then 
-					atram.py -b ${f%.*.*.*.*.*.*} -q $f -a velvet -o exon -i 10;
+					atram.py -b ${directory%/} -q $f -a velvet -o exon -i 10;
 				fi;
 			done;
 		gzip *fastq;	
