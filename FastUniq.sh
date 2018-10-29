226fb9aa020b8472dee2bae6fb3e2199663de624
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2018-10-28 21:42:36 -0400

    fixed error

--- FastUniq.sh
+++ FastUniq.sh
@@ -4,18 +4,16 @@ for directory in *;
 do
 if [  -d $directory  ];
 then
-taxon=$(echo ${directory%/})
-echo $taxon;
 if [  ! -e $directory.dedup.txt  ];
 then
 echo $directory deduplication started > $directory.dedup.txt;
-cd $directory/;
-gunzip *trimmed.fastq.gz;
-ls *.trimmed.fastq > inputlist.txt;
-../../FastUniq/source/fastuniq -i inputlist.txt -o $taxon_R1.trimmed_dedup.fastq -p $taxon_R2.trimmed_dedup.fastq;
-gzip *trimmed.fastq;
-cd ../;
+gunzip $directory/*trimmed.fastq.gz;
+ls $directory/*.trimmed*
+ls $directory/*.trimmed.fastq > $directory/inputlist.txt;
+../FastUniq/source/fastuniq -i $directory/inputlist.txt -o $directory/${directory%/}_R1.trimmed_dedup.fastq -p $directory/${directory%/}_R2.trimmed_dedup.fastq;
+gzip $directory/*trimmed.fastq;
 echo $directory deduplication completed > $directory.dedup.txt;
 fi;
 fi;
 done
+
