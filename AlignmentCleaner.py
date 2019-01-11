c2b31d475df743b5a3c105eb5318d635a74f50fb
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2019-01-11 14:23:07 -0500

    updated to filter mitochondrial stop codons

--- AlignmentCleaner.py
+++ AlignmentCleaner.py
@@ -14,7 +14,8 @@ from Bio.Alphabet import IUPAC
 parser = argparse.ArgumentParser(description="Requires python 2.7 and Biopython. This script cleans alignments for single-taxon insertions, gappy sequences, more than one stop codon, and converts a single stop codon to 'NNN'. Remove of gappy sequences is controlled with the -c parameter.")
 parser.add_argument('-f', '--fasta' , dest = 'fasta' , type = str , default= None , required= True, help = 'Fasta alignment to clean.')
 parser.add_argument('-o', '--output', dest = 'output', type = str, default = None, required = True, help = 'Name of output file.')
-parser.add_argument('-c', '--coverage', dest = 'coverage', type = float, default = 0.50, required = False, help = 'Maximum proportion of gaps allowed in a sequence. Default = 0.50')
+parser.add_argument('-c', '--coverage', dest = 'coverage', type = float, default = 0.50, required = True, help = 'Maximum proportion of gaps allowed in a sequence. Default = 0.50')
+parser.add_argument('-m', '--mitochondrial', dest = 'mitochondrial', type = str, default = 'False', required = True, help = 'If set to True, replaces single stop codons with NNN using Vertebrate Mitochondrial genetic code instead of the standard genetic code. Default = False')
 args, unknown = parser.parse_known_args()
 
 # read in the alignment in fasta format
@@ -30,54 +31,82 @@ numTaxa = len(alignment)
 goodColumns = []
 
 for x in range(0,length):
-	column = alignment[:,x]
-	if column.count("-") < (numTaxa-3):
-		slice = alignment[:,x:x+1]
-		goodColumns.append(slice)
+    column = alignment[:,x]
+    if column.count("-") < (numTaxa-3):
+        slice = alignment[:,x:x+1]
+        goodColumns.append(slice)
 
 
 # make an empty alignment to populate
-		
+
 goodColumnsAlignment = alignment[:,0:0]
 for column in goodColumns:
-	goodColumnsAlignment = goodColumnsAlignment+column
+    goodColumnsAlignment = goodColumnsAlignment+column
 
 newlength = goodColumnsAlignment.get_alignment_length()
 
-# drop taxa with more than the allowed percentage of NNNs, and more than 1 stop codon
+# drop taxa with more than the allowed percentage of NNNs, and more than 1 stop codon, and replace single stop codons with 'NNN'
 goodTaxa = []
 
 def RemoveStops(sequence):
-    """ Replaces stop codons (standard genetic code) with 'NNN' in a nucleotide seq object and returns a new seq object"""
-    TAG = 'TAG'
-    TGA = 'TGA'
-    TAA = 'TAA'
+    """ Replaces stop codons (standard or vertebrate mitochondrial genetic code) with 'NNN' in a nucleotide seq object and returns a new seq object"""
     clean_seq = Seq('')
     codons = [sequence[x:x+3] for x in range(0, len(sequence), 3)]
-    for codon in codons:
-        if TAG in codon or TGA in codon or TAA in codon:
-            clean_seq = clean_seq+Seq("NNN")
-        else:
-            clean_seq = clean_seq+codon
+    if 'True' in args.mitochondrial:
+        for codon in codons:
+            if '---' in codon :
+                clean_seq = clean_seq+codon
+            elif codon.translate(table="Vertebrate Mitochondrial") == '*':
+                clean_seq = clean_seq+Seq("NNN")
+            else:
+                clean_seq = clean_seq+codon
+    else:
+        for codon in codons:
+            if '---' in codon:
+                clean_seq = clean_seq+codon
+            elif codon.translate() == '*':
+                clean_seq = clean_seq+Seq("NNN")
+            else:
+                clean_seq = clean_seq+codon
     return clean_seq
 
+def StopCounter(sequence):
+    """Counts the number of stop codons in a Seq object for standard or vertebrate mitochondrial genetic code"""
+    count = 0
+    codons = [sequence[x:x+3] for x in range(0, len(sequence), 3)]
+    if 'True' in args.mitochondrial:
+        for codon in codons:
+            if '---' in codon :
+                pass
+            elif codon.translate(table="Vertebrate Mitochondrial") == '*':
+                count = count+1
+            else:
+                pass
+    else:
+        for codon in codons:
+            if '---' in codon :
+                pass
+            elif codon.translate() == '*':
+                count = count+1
+            else:
+                pass
+    return(count)
+
+
 for record in goodColumnsAlignment:
-	seq = record.seq
-	gaps = seq.count("-")
-	Ns = seq.count("N")
-	transeq = str(record.seq).replace("-","N")
-	transeqrec = Seq(transeq)
-	translated = transeqrec.translate()
-	stops = translated.count("*")
-	if gaps < (newlength*args.coverage) and Ns < 4:
-		if stops < 1:
-			goodTaxa.append(record)
-		elif stops == 1:
-			replace_stops = RemoveStops(seq)
-			new_record = SeqRecord(replace_stops, id=record.id, description='')
-			goodTaxa.append(new_record)
-		elif stops > 1:
-			pass	
+    seq = record.seq
+    gaps = seq.count("-")
+    Ns = seq.count("N")
+    stops = StopCounter(seq)
+    if gaps < (newlength*args.coverage) and Ns < 4:
+        if stops < 1:
+            goodTaxa.append(record)
+        elif stops == 1:
+            replace_stops = RemoveStops(seq)
+            new_record = SeqRecord(replace_stops, id=record.id, description='')
+            goodTaxa.append(new_record)
+        elif stops > 1:
+            pass    
 
 TaxAlignment =  MultipleSeqAlignment(goodTaxa)
 
