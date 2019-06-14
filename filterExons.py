18517a7fd6f71d93472df5318e6db369037e72ee
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2019-06-14 12:37:35 -0400

    simplified code

--- filterExons.py
+++ filterExons.py
@@ -9,14 +9,11 @@ import Bio
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
-from Bio.Alphabet import IUPAC
 
-parser = argparse.ArgumentParser(description="Requires python 2.7 and Biopython. Filters the longest, highest coverage exon from the exonerate output, and renames the sequence simply as >Taxon")
+parser = argparse.ArgumentParser(description="Requires python 2.7 and Biopython. Filters the exonerate output for sequences on the correct strand, and renames the sequence simply as >Taxon")
 parser.add_argument('-f', '--fasta' , dest = 'fasta' , type = str , default= None , required= True, help = 'Fasta alignment to prune')
 parser.add_argument('-o', '--output', dest = 'output', type = str, default = None, required = True, help = 'Name of output file')
 parser.add_argument('-t', '--taxon', dest = 'taxon', type = str, default = None, required = True, help = 'Name of taxon')
-parser.add_argument('-l', '--length', dest = 'length', type = int, default = 100, required = True, help = 'Minimum sequence length to write to file. Default = 100 bp')
-parser.add_argument('-m', '--mito', dest = 'mito', type = str, default = 'False', required = False , help = 'If set to True, uses vertebrate mitochondiral genetic code. Default = False')
 
 args, unknown = parser.parse_known_args()
 
@@ -25,7 +22,6 @@ args, unknown = parser.parse_known_args()
 input = open(args.fasta)
 records = list(SeqIO.parse(input, "fasta"))
 input.close()
-records.sort(key=lambda r: -len(r))
 
 # filter just the sequences in the correct orientation (known from the reference sequence used with exonerate)
 oriented = []
@@ -37,15 +33,9 @@ for record in records:
 	if end > start:
 		oriented.append(record)	
 
-# filter for length
-longest = []
+filtered = []
 
-if len(oriented) > 0:
-	for record in oriented:
-		if len(record.seq) >= args.length:
-			longest.append(record)
+for record in oriented:
+	filteredSeq = SeqRecord(record.seq, id=args.taxon, description='')
 
-longest.sort(key=lambda r: -len(r))
-
-filteredSeq = SeqRecord(longest[0].seq, id=args.taxon, description='')
-SeqIO.write(filteredSeq, args.output, "fasta")
+SeqIO.write(filtered, args.output, "fasta")
