e95e3757792d6ef0eaa002b41dc08da69ca42562
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2019-07-02 14:33:01 -0400

    script to get correct orientation of flanking sequences

---
+++ filterFlanks.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import re
+from sys import argv
+import argparse
+import os
+import sys
+
+import Bio
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+parser = argparse.ArgumentParser(description="Requires python 2.7 and Biopython. Filters the exonerate output for sequences on the correct strand, and renames the sequence simply as >Taxon")
+parser.add_argument('-f', '--fasta' , dest = 'fasta' , type = str , default= None , required= True, help = 'Fasta alignment to prune')
+parser.add_argument('-o', '--output', dest = 'output', type = str, default = None, required = True, help = 'Name of output file')
+parser.add_argument('-t', '--taxon', dest = 'taxon', type = str, default = None, required = True, help = 'Name of taxon')
+parser.add_argument('-l', '--flanks' dest = 'flanks', type = str, default = None, required = False, help = 'Complete assembled sequences')
+
+args, unknown = parser.parse_known_args()
+
+
+# read in sequences
+input = open(args.fasta)
+records = list(SeqIO.parse(input, "fasta"))
+input.close()
+
+
+# filter just the sequences in the correct orientation (known from the reference sequence used with exonerate)
+oriented = []
+	
+for record in records:
+	coordinates = record.description.split("\t")[1]
+	start = int(coordinates.split("-")[0])
+	end = int(coordinates.split("-")[1])
+	if end > start:
+		oriented.append(record)	
+
+if 'flanks' not in sys.argv:
+	
+	filtered = []
+
+	for record in oriented:
+		filteredSeq = SeqRecord(record.seq, id=args.taxon, description='')
+		filtered.append(filteredSeq)
+
+	SeqIO.write(filtered, args.output, "fasta")
+	
+else:
+	
+	WSdict = Bio.SeqIO.to_dict(SeqIO.parse(args.flanks,"fasta"))
+	
+	genomic = []
+	
+	for sequence in oriented:
+		genomicSeq = WSdict[sequence.id]
+		if sequence.seq in genomicSeq.seq:
+			newRecord = SeqRecord(genomicSeq.seq, id=args.taxon, description='')
+			genomic.append(newRecord)
+		else:
+			genomicSeqRev = genomicSeq.seq.reverse_complement()	
+			newRecord = SeqRecord(genomicSeqRev, id=args.taxon, description='')
+			genomic.append(newRecord)
+			
+		
+
+
+
