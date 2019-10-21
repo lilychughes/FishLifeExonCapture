0bd22dbbd66bf357ae7f80f193cb8ea9f5bd3675
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2019-10-21 15:35:27 -0400

    fixed error

--- map-exons.sh
+++ map-exons.sh
@@ -1161,11 +1161,7 @@ if [  -s $directory/${directory%/}.rmdup.fastq  ];
 then
 echo Mapping completed $directory >> ${directory%/}.step2.readmapping.txt;
 else
-	if [  ! -s $directory/${directory%/}_R1.trimmed.fastq.gz  ];
-	then
-	echo Mapping was not completed. Check that Step1 (Trimming) ran first! >> ${directory%/}.step2.readmapping.txt;
-	else
-	echo Mapping was not completed. Check that all dependencies (BWA, Samtools 1.7 or higher) are installed >> ${directory%/}.step2.readmapping.txt;
-	fi;
+echo Mapping was not completed. Check that all dependencies (BWA, Samtools 1.7 or higher) are installed >> ${directory%/}.step2.readmapping.txt;
+fi;
 fi;
 done
