a59102ee1668aac79b72704444cfafc307f52231
Lily Hughes <lilychughes@Lilys-MacBook-Pro.local>
2019-10-21 14:10:42 -0400

    fixed error, now redirects stout and sterr to the .txt files

--- trimmomatic-loop-PE.sh
+++ trimmomatic-loop-PE.sh
@@ -25,7 +25,7 @@ then
 	cd $directory/;
 		for fastq in *R1.fastq.gz;
 		do
-		java -jar $jarfile PE -threads 4 -phred33 -trimlog $directory.trimlog $fastq ${fastq%_*.*.*}_R2.fastq.gz ${fastq%_*.*.*}_R1.trimmed.fastq.gz ${fastq%_*.*.*}_rem1.fastq.gz ${fastq%_*.*.*}_R2.trimmed.fastq.gz ${fastq%_*.*.*}_rem2.fastq.gz ILLUMINACLIP:$adapters:2:30:10 LEADING:5 TRAILING:5 SLIDINGWINDOW:4:15 MINLEN:31 >> ../$directory.step1.trimming.txt;
+		java -jar $jarfile PE -threads 4 -phred33 -trimlog $directory.trimlog $fastq ${fastq%_*.*.*}_R2.fastq.gz ${fastq%_*.*.*}_R1.trimmed.fastq.gz ${fastq%_*.*.*}_rem1.fastq.gz ${fastq%_*.*.*}_R2.trimmed.fastq.gz ${fastq%_*.*.*}_rem2.fastq.gz ILLUMINACLIP:$adapters:2:30:10 LEADING:5 TRAILING:5 SLIDINGWINDOW:4:15 MINLEN:31 > ../$directory.step1.trimming.txt 2>&1;
 			if [  -s ${fastq%_*.*.*}_R1.trimmed.fastq.gz  ];
 			then
 			echo Trimming completed >> ../$directory.step1.trimming.txt;
