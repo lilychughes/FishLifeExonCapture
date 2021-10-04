dae9d62a9421a314f06f972154bd3ce3b5563179
lilychughes <lilychughes@gmail.com>
2021-10-04 15:46:35 -0400

    Added alphabet argument
    Fixes issue with converting nexus files

--- scripts/AlignmentConverter.py
+++ scripts/AlignmentConverter.py
@@ -3,20 +3,21 @@
 from sys import argv
 import argparse
 from Bio import AlignIO
+from Bio.Alphabet import Gapped
+from Bio import Alphabet
 
-parser = argparse.ArgumentParser(description="Requires python 2.7 and Biopython 1.48 or higher. ")
+parser = argparse.ArgumentParser(description="Requires python 2.7 or 3 and Biopython 1.48 or higher. Use 'phylip-relaxed' if you have taxon names longer than 10 characters or they will be truncated. ")
 parser.add_argument('-f', '--file' , dest = 'file' , type = str , default= None , required= True, help = 'Alignment file')
 parser.add_argument('-i', '--infmt' , dest = 'infmt', type = str, default= None, required= True, help = 'Input format of alignment file')
 parser.add_argument('-o', '--outfmt' , dest = 'outfmt', type = str, default= None, required= True, help = 'Output format of alignment file')
 parser.add_argument('-n', '--outfile', dest = 'outfile', type = str, default=None, required= True, help = 'Name of file to write re-formatted alignment to')
-
 args, unknown = parser.parse_known_args()
 
 input = open(args.file)
 
 output = open(args.outfile, "w")
 
-alignment = AlignIO.parse(input, args.infmt)
+alignment = AlignIO.parse(input, args.infmt, alphabet=Alphabet.generic_dna)
 AlignIO.write(alignment, args.outfile, args.outfmt)
 
 input.close()
